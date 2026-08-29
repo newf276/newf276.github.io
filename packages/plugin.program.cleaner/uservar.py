362cd0427a10d30036799e5e74acf9d774439c05
Newf <ian@kellys.me>
2026-08-29 15:45:04 -0700

    dgefg

---
+++ packages/plugin.program.cleaner/uservar.py
@@ -0,0 +1,14 @@
+'''#####-----Build File-----#####'''
+buildfile = 'https://www.dropbox.com/scl/fi/vrdxxv76qvdce1kvklvpt/builds.xml?rlkey=86un9y8o1l3xnsvugzcndp494&dl=1'
+
+'''#####-----Videos File-----#####'''
+videos_url = 'https://youtu.be/DO_W22vggGE'
+
+'''#####-----Notification File-----#####'''
+notify_url  = ''
+
+'''#####-----Changelog Directory-----#####'''
+changelog_dir  = 'https://github.com/%s/%s/raw/master/packages/%s'
+
+'''#####-----Excludes-----#####'''
+excludes  = ['plugin.video.whatever']
